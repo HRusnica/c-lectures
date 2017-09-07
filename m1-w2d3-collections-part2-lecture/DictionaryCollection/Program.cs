b38635dc4377f5510ea57acffff8ad1e07bb7ac1
Josh Tucholski <josh@techelevator.com>
2017-09-06 21:27:47 -0400

    Added Collections Part 2 Lecture Code

---
+++ m1-w2d3-collections-part2-lecture/DictionaryCollection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Person / Height Database");
+            Console.WriteLine();
+
+            Console.Write("Would you like to enter another person (yes/no)? ");
+            string input = Console.ReadLine().ToLower();
+
+            while (input == "yes" || input == "y")
+            {
+                
+
+                Console.Write("Would you like to enter another person (yes/no? ");
+                input = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(".... printing ...");
+        }
+    }
+}
