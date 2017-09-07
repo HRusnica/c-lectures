2ad1ab8d108becf3172982d30467c3577703089f
Josh Tucholski <josh@techelevator.com>
2017-09-07 12:13:53 -0400

    Added Collections Part 2 Lecture Code

--- m1-w2d3-collections-part2-lecture/DictionaryCollection/Program.cs
+++ m1-w2d3-collections-part2-lecture/DictionaryCollection/Program.cs
@@ -13,19 +13,63 @@ namespace DictionaryCollection
             Console.WriteLine("Welcome to the Person / Height Database");
             Console.WriteLine();
 
-            Console.Write("Would you like to enter another person (yes/no)? ");
+            Console.Write("Would you like to enter a person (yes/no)? ");
             string input = Console.ReadLine().ToLower();
 
+            // Declare and instantiate a new dictionary
+            Dictionary<string, double> peopleHeights = new Dictionary<string, double>();
+            
             while (input == "yes" || input == "y")
             {
-                
+                // Get the name
+                Console.Write("Please enter a name: ");
+                string name = Console.ReadLine();
 
-                Console.Write("Would you like to enter another person (yes/no? ");
+                // Get their height
+                Console.Write("Please enter their height: ");
+                double height = double.Parse(Console.ReadLine());
+
+                // Add the student's name and height to the dictionary
+                peopleHeights[name] = height;
+
+                Console.WriteLine("Thank you there are now " + peopleHeights.Count + " people recorded.");
+
+                Console.WriteLine();
+                Console.Write("Would you like to enter another person (yes/no?) ");
                 input = Console.ReadLine().ToLower();
             }
 
+            // Looping through a Dictionary
+            foreach (KeyValuePair<string, double> kvp in peopleHeights)
+            {
+                string name = kvp.Key; //<-- student's name
+                double h = kvp.Value; //<-- student's height
+                Console.WriteLine(name + " has a height of " + h + " inches tall.");
+            }
+
+
+            
             Console.WriteLine();
-            Console.WriteLine(".... printing ...");
+            Console.Write("Whose height do you want to retrieve? ");
+            string studentName = Console.ReadLine();
+
+            double studentHeight = peopleHeights[studentName]; //<-- gets the value out of the dictionary
+
+            Console.WriteLine(studentName + " is " + studentHeight + " inches tall.");
+
+
+            Console.WriteLine();
+            Console.Write("Who do you want to remove? ");
+            string studentToRemove = Console.ReadLine();
+
+            Console.WriteLine("There are now " + peopleHeights.Count + " people recorded.");
+
+            peopleHeights.Remove(studentToRemove);
+
+            
+
+            Console.WriteLine("There are now " + peopleHeights.Count + " people recorded.");
+
         }
     }
 }
---
+++ m1-w2d3-collections-part2-lecture/DictionaryCollection/Reference/Backup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCollection.Reference
+{
+    class Backup
+    {
+        public static void DoBackup()
+        {
+            Console.WriteLine("Welcome to the Person / Height Database");
+            Console.WriteLine();
+
+            Console.Write("Would you like to enter person (yes/no)? ");
+            string input = Console.ReadLine().ToLower();
+
+            List<string> names = new List<string>(); //<-- student names
+            List<double> heights = new List<double>(); //<-- student heights
+
+            while (input == "yes" || input == "y")
+            {
+                // Get the name
+                Console.Write("Please enter a name: ");
+                string name = Console.ReadLine();
+                names.Add(name);
+
+                // Get their height
+                Console.Write("Please enter their height: ");
+                double height;
+                string inputValue = Console.ReadLine();
+                if (double.TryParse(inputValue, out height)) //<-- they typed in a valid height
+                {
+                    heights.Add(height);
+                }
+                else
+                {
+                    heights.Add(-1);
+                }
+
+
+                Console.WriteLine();
+                Console.Write("Would you like to enter another person (yes/no?) ");
+                input = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine();
+            Console.Write("Whose height do you want to retrieve? ");
+            string studentName = Console.ReadLine();
+
+            int indexOfStudent = -1; // names.IndexOf(studentName);
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (names[i] == studentName)
+                {
+                    indexOfStudent = i;
+                    break;
+                }
+            }
+            Console.WriteLine(names[indexOfStudent] + " is " + heights[indexOfStudent] + " inches tall.");
+
+
+            //Mandy is 65 inches tall
+            // Teddi is 62 inches tall
+            // Lindsay is 71 inches tall
+            //for (int i = 0; i < names.Count; i++)
+            //{
+            //    if (heights[i] == -1)
+            //    {
+            //        Console.WriteLine(names[i] + " - height not available.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(names[i] + " is " + heights[i] + " inches tall.");
+            //    }
+            //}
+        }
+    }
+}
