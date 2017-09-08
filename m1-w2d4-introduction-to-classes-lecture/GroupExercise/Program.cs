036fe85a8a333ed3697f190c4b4a74072064e669
Josh Tucholski <josh@techelevator.com>
2017-09-07 22:41:02 -0400

    Added intro to classes lecture code

---
+++ m1-w2d4-introduction-to-classes-lecture/GroupExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Group exercise:
+            // Let's design a class
+            // After we design the class, let's practice instantiating it             
+        }
+    }
+}
