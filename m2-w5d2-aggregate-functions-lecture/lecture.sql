57bbd7f7138bc4fa6ee7a226694acfb80493e224
Josh Tucholski <josh@techelevator.com>
2017-09-25 21:48:05 -0400

    Added lecture code for w5d2.

---
+++ m2-w5d2-aggregate-functions-lecture/lecture.sql
@@ -0,0 +1,64 @@
+-- ORDERING RESULTS
+
+-- Populations of all countries in descending order
+
+
+--Names of countries and continents in ascending order
+
+
+
+
+
+-- LIMITING RESULTS
+-- The name and average life expectancy of the countries with the 10 highest life expectancies.
+
+-- SQL
+
+
+
+
+-- CONCATENATING OUTPUTS
+
+-- The name & state of all cities in California, Oregon, or Washington. 
+-- "city, state", sorted by state then city
+
+-- SQL
+
+
+
+-- AGGREGATE FUNCTIONS
+-- Average Life Expectancy in the World
+
+-- Total population in Ohio
+
+-- The surface area of the smallest country in the world
+
+-- The 10 largest countries in the world
+-- SQL
+
+-- The number of countries who declared independence in 1991
+
+
+
+-- GROUP BY EXERCISES
+-- Count the number of countries where each language is spoken, order show them from most countries to least
+
+-- Average life expectancy of each continent ordered from highest to lowest
+
+-- Exclude Antarctica from consideration for average life expectancy
+
+-- Sum of the population of cities in each state in the USA ordered by state name
+
+-- The average population of cities in each state in the USA ordered by state name
+
+
+-- Ordering allows columns to be displayed in ascending order, or descending order (Look at Arlington)
+
+
+-- Limiting results allows rows to be returned in 'limited' clusters where LIMIT says how many, and an optional OFFSET specifies number of rows to skip
+
+
+-- Aggregate functions provide the ability to COUNT, SUM, AVG, and determine MIN and MAX. Only returns a single row of value(s).
+
+-- GROUP BY clusters rows by a column value
+
