0e514310a5ba2432a3e68d3a4e6a7da6d13c456a
Josh Tucholski <josh@techelevator.com>
2017-10-03 14:57:30 -0400

    Pushed lecture code.

---
+++ m2-w6d2-integration-tests/RecipeManager.CLI.Tests/IngredientDALTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeManager.CLI.Models;
+using RecipeManager.CLI.DAL;
+using System.Transactions;
+using System.Data.SqlClient;
+
+
+namespace RecipeManager.CLI.Tests
+{
+    [TestClass]
+    public class IngredientDALTests
+    {
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=cookbook;User ID=te_student;Password=sqlserver1";
+        TransactionScope t;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Code that runs before each test
+            // Begin a new transaction
+            t = new TransactionScope();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Code that runs after each test
+            // Rollback the transaction by calling .Dispose()
+            t.Dispose();
+        }
+
+        [TestMethod]
+        public void AddIngredientToExistingRecipe()
+        {
+            // Arrange
+            // - Add a Recipe so that we can add ingredients
+            //      - Write SQL code to Insert FAKE RECIPE
+            int recipeId;
+
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO recipe VALUES ('Fake Recipe', 'Fake Instructions', 'Fake Description', 0);", conn);
+                cmd.ExecuteNonQuery();
+
+                //      - Get new recipe Id with SELECT SCOPE_IDENTITY()
+                cmd = new SqlCommand("SELECT CAST(SCOPE_IDENTITY() as int);", conn);
+                recipeId = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            // - Create an ingredient to add
+            Ingredient i = new Ingredient();
+            i.Name = "FAKE INGREDIENT";
+            i.Quantity = "FAKE QUANTITY";
+            i.RecipeId = recipeId;
+
+            // - Create the ingredient DAL
+            IngredientDAL dal = new IngredientDAL(connectionString);
+
+            // Act
+            // - Call AddIngredient in the IngredientDAL
+            bool output = dal.AddIngredient(i);
+
+
+            // Assert
+            // - Validate we received TRUE from AddIngredient
+            Assert.IsTrue(output);
+
+        }
+    }
+}
---
+++ m2-w6d2-integration-tests/RecipeManager.CLI.Tests/RecipeDALTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeManager.CLI.Models;
+using RecipeManager.CLI.DAL;
+using System.Transactions;
+
+
+namespace RecipeManager.CLI.Tests
+{
+    [TestClass]
+    public class RecipeDALTests
+    {
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=cookbook;User ID=te_student;Password=sqlserver1";
+        TransactionScope t;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Code that runs before each test
+            // Begin a new transaction
+            t = new TransactionScope();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // Code that runs after each test
+            // Rollback the transaction by calling .Dispose()
+            t.Dispose();
+        }
+
+
+
+
+        [TestMethod]
+        public void AddRecipeToDatabaseTest()
+        {
+            //Arrange
+            // - Fake Recipe
+            Recipe r = new Recipe();
+            r.CookTimeInMinutes = 10;
+            r.Description = "FAKE DESCRIPTION";
+            r.Instructions = "FAKE INSTRUCTIONS";
+            r.Name = "FAKE NAME";
+
+            // - DAL to Insert the Fake Recipe
+            RecipeDAL dal = new RecipeDAL(connectionString);
+
+            //Act
+            // - Call AddRecipe using the Fake Recipe
+            bool output = dal.AddRecipe(r);
+
+            //Assert
+            // - Validatea that AddRecipe returned TRUE
+            Assert.IsTrue(output);
+
+        }
+    }
+}
