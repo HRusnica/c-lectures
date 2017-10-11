e3af136e813716bc7f90697bc7a9326fe779434d
Josh Tucholski <josh@techelevator.com>
2017-10-11 12:31:10 -0400

    Added lecture code.

---
+++ m3-w7d3-intro-to-mvc/IntroToMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroToMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // GET: Home/Index
+        public string Index()
+        {
+            return "Hello World!";
+        }
+
+        // GET: Home/DateTime <-- URL to open to call this action
+        public string DateTime()
+        {
+            return System.DateTime.Now.ToString();
+        }
+
+        // GET: Home/Stuff <-- URL to invoke this action
+        public ActionResult Stuff()
+        {
+            return View("Stuff");
+        }
+
+        public ActionResult Message()
+        {
+            return View("Message");
+        }
+
+        public ActionResult MetricToImperial()
+        {
+            return View("MetricToImperial");
+        }
+
+
+    }
+}
