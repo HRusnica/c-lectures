c5f050869708104c268388706f84e7d2024d140f
Josh Tucholski <josh@techelevator.com>
2017-10-16 22:12:47 -0400

    Added lecture code.

---
+++ m3-w8d2-controllers-part2-lecture/FlyByNightBank.Web/Controllers/SurveyController.cs
@@ -0,0 +1,18 @@
+﻿using FlyByNightBank.Web.DAL;
+using FlyByNightBank.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlyByNightBank.Web.Controllers
+{
+    public class SurveyController : Controller
+    {
+        
+
+        
+    }
+}
