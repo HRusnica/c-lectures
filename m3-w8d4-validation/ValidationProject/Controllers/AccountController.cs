40775521d9e0e8e8d293bfeffb15e629b2fe810c
Josh Tucholski <josh@techelevator.com>
2017-10-19 11:43:13 -0400

    Added validation lecture code.

---
+++ m3-w8d4-validation/ValidationProject/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidationProject.Models;
+
+namespace ValidationProject.Controllers
+{
+    //1. Create controller
+    public class AccountController : Controller
+    {
+
+        //2. Create a Register Action to return the Empty Form
+        [HttpGet] //<-- this is irrelevant because Get is the default
+        public ActionResult Register()
+        {
+            // 2. Return Register View for Register Action
+            //return View("Register", );
+
+            //10. Create the RegisterModel and pass to the View
+            RegisterModel model = new RegisterModel();
+            return View("Register", model);
+        }
+
+        //3. Create a Register Action (POST) to process the Form
+        //  -> Go to RegisterModel for #4
+        [HttpPost]
+        public ActionResult Register(RegisterModel model) //6. Pass the model into the action
+        {
+            //3. Create Register Action (POST)
+            //RegisterModel input = model; <-- this line can be "dead to us"
+            //return RedirectToAction("ThankYou");
+            
+            //14. Implement Validation Enforcement and then TEST
+            if (!ModelState.IsValid)
+            {
+                return View("Register", model); // <-- Send back to the Register View
+                                                //      with their previous entered data
+            }
+            else
+            {
+                // Send them to the ThankYou page
+                return RedirectToAction("ThankYou");
+            }
+        }
+
+        //7. Create the ThankYou Action to display a Thank You Page
+        //  -> Go Create Views Register & ThankYou
+        public ActionResult ThankYou()
+        {
+            return View("ThankYou");
+        }
+    }
+}
---
+++ m3-w8d4-validation/ValidationProject/Models/RegisterModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ValidationProject.Models
+{
+    //4. Create a model that will hold our data
+    public class RegisterModel
+    {
+        //5. Add the properties
+        //public string Email { get; set; }
+        //public string Password { get; set; }
+        //public string ConfirmPassword { get; set; }
+
+        //13. Add Validation Rules
+        //      then go enforce validation in controller
+        [Required(ErrorMessage = "Enter your email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "You need a password")]
+        [RegularExpression("^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$", ErrorMessage = "your password sucks")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
