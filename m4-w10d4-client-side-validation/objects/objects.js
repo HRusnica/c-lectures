fae5303f916d73c03e32ed1b41645e928d23e7e6
Josh Tucholski <josh@techelevator.com>
2017-11-02 12:23:11 -0400

    Added client side validation code.

--- m4-w10d4-client-side-validation/objects/objects.js
+++ m4-w10d4-client-side-validation/objects/objects.js
@@ -1,7 +1,35 @@
 ﻿//1. Let's practice creating objects
 
 //1a. Creating an object literal
+var person = {
+    firstName: "Josh",
+    lovesJavascript: true,
+    cars: ["Subaru", "Chevy"],
+    greeting: function () {
+        return "Who misses C#?";
+    }
+};
+
+//flexible but sloppy
+var person2 = {
+    fName: "John",
+    lovesJS: false,
+}
 
 
+// loop through the properties on person
+for (var i in person) {
+    console.log(i + " is " + person[i]);
+}
+
+
+//person.teaches = "C#";
 
 //1b. Creating a "class"
+function Person(name, lovesJs) {
+    this.firstName = name;
+    this.lovesJavascript = lovesJs;
+}
+
+var josh = new Person("Josh", true);
+var marty = new Person("Marty", false);
--- m4-w10d4-client-side-validation/validation.js
+++ m4-w10d4-client-side-validation/validation.js
@@ -1,6 +1,48 @@
 $(function () {
 
+    //8. Let me figure this out and post it
+    $("#chkEmail").on("change", function (e) {
+        $("#applicationForm").valid();
+    });
+
     // Validate takes an object, not a function
     // Objects in javascript use { .. } notation and are the same as key / value pairs
-
+    // 1. Wire up the validation rules for the #applicationForm
+    $("#applicationForm").validate({
+        rules: {
+            //2. Add a rule for fName field
+            fName: {
+                required: true, //<-- makes it required
+                maxlength: 20,                
+            },
+            password: {
+                martysRule: true //<-- 5. Make our own rule
+            },
+            //7. Add a conditional rule if the contact box is checked
+            emailAddr: {
+                required: {
+                    depends: function (element) {
+                        return $("#chkEmail").is(":checked");
+                    }
+                }
+            }
+        },
+        //4. Add a custom message for maxlength
+        messages: {
+            fName: {
+                maxlength: "Your first name is too long"
+            }
+        },
+        //3. Define a custom error class
+        //      and define custom styles
+        errorClass: "error"
+    });
 });
+
+//6. Implement Martys Rule
+//Create a custom validation rule that only permits email addresses that end with @techelevator.com
+//https://jqueryvalidation.org/jQuery.validator.addMethod
+$.validator.addMethod("martysRule", function (value) {
+    // must return true or false
+    return value.match(/[A-Z]/) && value.match(/[a-z]/) && value.match(/\d/);  //check for one capital letter, one lower case letter, one num
+}, "Password is too weak for Marty");
