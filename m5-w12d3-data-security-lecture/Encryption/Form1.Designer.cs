51dfa11d9bec1a38668b38c742b6d7cfbf853da4
Josh Tucholski <josh@techelevator.com>
2017-11-14 22:44:59 -0500

    Added data security lecture.

---
+++ m5-w12d3-data-security-lecture/Encryption/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.TextBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.key = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 458);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(938, 190);
+            this.output.TabIndex = 0;
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 48);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(938, 175);
+            this.input.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(58, 382);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(125, 50);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(660, 382);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(125, 50);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate Public/Private Key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(12, 272);
+            this.key.Multiline = true;
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(938, 85);
+            this.key.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Paste Public or Private Key Here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Paste Message Here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 669);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ m5-w12d3-data-security-lecture/Encryption/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+
+            string filePath = openFileDialog1.FileName;
+            string cipherText = input.Text;
+            string privateKey = key.Text;
+
+            string plainText = DecryptCipherText(cipherText, privateKey);
+
+            output.Text = plainText;            
+        }
+
+
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+
+            string filePath = openFileDialog1.FileName;
+            string plainText = input.Text;
+            string publicKey = key.Text;
+
+            string cipherText = GetCipherText(plainText, publicKey);
+
+            output.Text = cipherText;
+
+        }
+
+        private string DecryptCipherText(string cipherText, string privateKey)
+        {
+            RSACryptoServiceProvider cipher = new RSACryptoServiceProvider();
+            cipher.FromXmlString(privateKey);
+            byte[] original = cipher.Decrypt(Convert.FromBase64String(cipherText), false);
+            return Encoding.UTF8.GetString(original);
+        }
+
+        private string GetCipherText(string plaintext, string publicKey)
+        {
+            RSACryptoServiceProvider cipher = new RSACryptoServiceProvider();
+            cipher.FromXmlString(publicKey);
+            byte[] data = Encoding.UTF8.GetBytes(plaintext);
+            byte[] cipherText = cipher.Encrypt(data, false);
+            return Convert.ToBase64String(cipherText);
+        }
+
+        private void Keys(string publicKeyFileName, string privateKeyFileName)
+        {
+
+            // Variables
+            CspParameters cspParams = null;
+            RSACryptoServiceProvider rsaProvider = null;
+            StreamWriter publicKeyFile = null;
+            StreamWriter privateKeyFile = null;
+
+            string publicKey = "";
+            string privateKey = "";
+
+            try
+            {
+
+                // Create a new key pair on target CSP
+                cspParams = new CspParameters();
+                cspParams.ProviderType = 1; // PROV_RSA_FULL
+                //cspParams.ProviderName; // CSP name
+
+                cspParams.Flags = CspProviderFlags.UseArchivableKey;
+                cspParams.KeyNumber = (int)KeyNumber.Exchange;
+                rsaProvider = new RSACryptoServiceProvider(cspParams);
+
+                // Export public key
+                publicKey = rsaProvider.ToXmlString(false);
+
+                // Write public key to file            
+                publicKeyFile = File.CreateText(publicKeyFileName);
+                publicKeyFile.Write(publicKey);
+
+
+                // Export private/public key pair
+                privateKey = rsaProvider.ToXmlString(true);
+
+                // Write private/public key pair to file
+                privateKeyFile = File.CreateText(privateKeyFileName);
+                privateKeyFile.Write(privateKey);
+
+            }
+            catch (Exception ex)
+            {
+
+                // Any errors? Show them
+
+                Console.WriteLine("Exception generating a new key pair! More info:");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                // Do some clean up if needed
+
+                if (publicKeyFile != null)
+                {
+                    publicKeyFile.Close();
+                }
+
+                if (privateKeyFile != null)
+                {
+                    privateKeyFile.Close();
+                }
+            }
+        } // Keys
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Keys("public.xml", "private.xml");
+        }
+    }
+}
